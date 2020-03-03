17623a1d894bb84dbb505a99a2c2cc6f390e53da
Kacper Ziubryniewicz <kapi2289@gmail.com>
2020-03-03 19:51:18 +0100

    Add discord webhook support and make Facebook client optional

--- src/_client.py
+++ src/_client.py
@@ -1,3 +1,5 @@
+import requests
+
 from datetime import datetime
 from time import sleep
 from typing import List
@@ -10,28 +12,33 @@ from fbchat.models import ThreadType, Message, Mention, LocationAttachment
 from ._settings import FB_GROUP_ID, TIMEZONE
 
 
-class Client(FbClient):
+class Client:
     eremiza_client: ERemizaClient
+    client: FbClient
     alarms: List[Alarm]
 
-    def __init__(self, email, password, session_cookies=None):
-        super(Client, self).__init__(email, password, session_cookies=session_cookies)
+    def __init__(self):
         self.alarms = list()
-        self.setDefaultThread(FB_GROUP_ID, ThreadType.GROUP)
+        self.client = None
+        self.group_id = None
+        self.wh_url = None
 
     def set_eremiza_client(self, client):
         self.eremiza_client = client
 
-    def listen(self, **kwargs):
-        self.startListening()
-        self.onListening()
+    def set_fbchat_client(self, client, group_id):
+        self.client = client
+        self.client.setDefaultThread(group_id, ThreadType.GROUP)
+        self.group_id = group_id
 
-        while self.listening and self.doOneListen():
+    def set_dc_webhook_url(self, url):
+        self.wh_url = url
+
+    def listen(self):
+        while self.eremiza_client and self.doOneListen():
             sleep(5)
 
-        self.stopListening()
-
-    def doOneListen(self, **kwargs):
+    def doOneListen(self):
         ids = list(map(lambda a: a.id, self.alarms))
         alarms: List[Alarm] = self.eremiza_client.get_alarms(count=10)
         now = datetime.now(TIMEZONE).replace(tzinfo=None)
@@ -57,12 +64,18 @@ class Client(FbClient):
                 alarm.dispatched_bsis_name,
             )
         )
-        group = self.fetchGroupInfo(FB_GROUP_ID)[FB_GROUP_ID]
-        mentions = [Mention(uid, offset=0, length=6) for uid in group.participants]
-        message = Message(text=msg, mentions=mentions)
-        if alarm.latitude and alarm.longitude:
-            self.sendLocation(
-                LocationAttachment(alarm.latitude, alarm.longitude), message=message
-            )
-        else:
-            self.send(message)
+        if self.client:
+            group = self.fetchGroupInfo(FB_GROUP_ID)[FB_GROUP_ID]
+            mentions = [Mention(uid, offset=0, length=6) for uid in group.participants]
+            message = Message(text=msg, mentions=mentions)
+            if alarm.latitude and alarm.longitude:
+                self.sendLocation(
+                    LocationAttachment(alarm.latitude, alarm.longitude), message=message
+                )
+            else:
+                self.send(message)
+        
+        if self.wh_url:
+            msg += "\n@everyone"
+            data = {"content": msg}
+            requests.post(self.wh_url, json=data)
--- start.py
+++ start.py
@@ -1,15 +1,22 @@
 from eremiza import Client as ERemizaClient
+from fbchat import Client as FbClient
 
 from src._client import Client
 from src._settings import (
     FB_EMAIL,
     FB_PASSWORD,
     FB_SESSION,
+    FB_GROUP_ID,
     EREMIZA_EMAIL,
     EREMIZA_PASSWORD,
+    DISCORD_WEBHOOK_URL,
 )
 
 if __name__ == "__main__":
-    client = Client(FB_EMAIL, FB_PASSWORD, session_cookies=FB_SESSION)
+    client = Client()
+    if ((FB_EMAIL and FB_PASSWORD) or FB_SESSION) and FB_GROUP_ID:
+        client.set_fbchat_client(FbClient(FB_EMAIL, FB_PASSWORD, session_cookies=FB_SESSION))
+    if DISCORD_WEBHOOK_URL:
+        client.set_dc_webhook_url(DISCORD_WEBHOOK_URL)
     client.set_eremiza_client(ERemizaClient(EREMIZA_EMAIL, EREMIZA_PASSWORD))
     client.listen()
